656ecc646c79d5fde92c418c4c656eb530b81e15
Manh Kha <manhkha.thai@gmail.com>
2024-09-22 19:14:35 +0700

    add next page and pre page

--- lib/graphql_pagination/collection_metadata_type.rb
+++ lib/graphql_pagination/collection_metadata_type.rb
@@ -5,5 +5,7 @@ module GraphqlPagination
     field :limit_value, Integer, null: false, description: "The number of items per page"
     field :total_count, Integer, null: false, description: "The total number of items to be paginated"
     field :total_pages, Integer, null: false, description: "The total number of pages in the pagination"
+    field :next_page, Integer, null: true, description: "Next Page"
+    field :prev_page, Integer, null: true, description: "Prev Page"
   end
 end
